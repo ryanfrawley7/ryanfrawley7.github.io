486dd46c0b5fc49de52fabb0edb8e029e3668617
ryanfrawley7 <15frawle7925@pit.edu>
2016-03-29 18:33:23 +0000

    First Commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//------------------------
+
+
+
+//------------------------
+window.onload = resizeRootEm;
+window.onresize = resizeRootEm;
+
+//------------------------
+function resizeRootEm(){
+    document.documentElement.style.fontSize = 10 + window.innerWidth/100 + "px";
+}
